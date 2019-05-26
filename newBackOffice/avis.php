402f3472b083cc0e4cf04198e8cad5e7ad475ba0
ErAmiot <46048736+ErAmiot@users.noreply.github.com>
2019-05-26 18:19:27 +0200

    Add files via upload

--- newBackOffice/avis.php
+++ newBackOffice/avis.php
@@ -2,11 +2,11 @@
 session_start();
 require '../sqlconnect.php';
 
-$isbn=$_POST['isbn'];
+$emprunt=$_POST['emprunt'];
 $avis='"'.$_POST['votreAvis'].'"';
 $user_id=$_SESSION['CLIENT_ID'];
 
-$sql = "UPDATE emprunter SET AVIS_EMPRUNTER = $avis WHERE LIV_ISBN = '$isbn' AND CLIENT_ID = $user_id";
+$sql = "UPDATE emprunter SET AVIS_EMPRUNTER = $avis WHERE EMP_ID = '$emprunt' AND CLIENT_ID = $user_id";
 echo $sql;
 $connection->query($sql);
 
